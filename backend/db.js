c8cace6007326a994e0ce7190d3a302c7eef8f58
saurav joshi <mesauravjoshii@gmail.com>
2025-10-24 12:12:03 +0530

    fix mongo url env

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const mongoURL = process.env.MONGO_URL
-const mongoURL = process.env.MONGO_LOCAL_URL
+const mongoURL = process.env.MONGO_URL
 
 async function connectDB() {
     try {
